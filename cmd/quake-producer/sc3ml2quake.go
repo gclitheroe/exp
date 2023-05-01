2e09c48a4b9a41c15415dda319b0c6a1485f03b2
Geoff Clitheroe <g.clitheroe@gmail.com>
2023-05-01 19:59:07 +1200

    Move bnechmarks, improve imports.

---
+++ cmd/quake-producer/sc3ml2quake.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"github.com/gclitheroe/exp/internal/quake"
+	"github.com/gclitheroe/exp/internal/seiscompml07"
+)
+
+/*
+fromSeiscompml07 converts seiscompml07.Event to a Quake.
+Only Arrivals and StationMagnitudeContribution that have contributed
+to Origins or Magnitudes (Weight > 0) are included in the Quake.
+*/
+func fromSeiscompml07(e seiscompml07.Event) quake.Quake {
+	mt := e.ModificationTime()
+
+	q := quake.Quake{
+		PublicID: e.PublicID,
+		Type:     e.Type,
+		Agency:   e.CreationInfo.AgencyID,
+		ModificationTime: &quake.Timestamp{
+			Seconds: mt.Unix(),
+			Nanos:   int64(mt.Nanosecond()),
+		},
+		Time: &quake.Timestamp{
+			Seconds: e.PreferredOrigin.Time.Value.Unix(),
+			Nanos:   int64(e.PreferredOrigin.Time.Value.Nanosecond()),
+		},
+		Latitude:              e.PreferredOrigin.Latitude.Value,
+		LatitudeUncertainty:   e.PreferredOrigin.Latitude.Uncertainty,
+		Longitude:             e.PreferredOrigin.Longitude.Value,
+		LongitudeUncertainty:  e.PreferredOrigin.Longitude.Uncertainty,
+		Depth:                 e.PreferredOrigin.Depth.Value,
+		DepthUncertainty:      e.PreferredOrigin.Depth.Uncertainty,
+		Method:                e.PreferredOrigin.MethodID,
+		EarthModel:            e.PreferredOrigin.EarthModelID,
+		EvaluationMode:        e.PreferredOrigin.EvaluationMode,
+		EvaluationStatus:      e.PreferredOrigin.EvaluationStatus,
+		UsedPhaseCount:        e.PreferredOrigin.Quality.UsedPhaseCount,
+		UsedStationCount:      e.PreferredOrigin.Quality.UsedStationCount,
+		StandardError:         e.PreferredOrigin.Quality.StandardError,
+		AzimuthalGap:          e.PreferredOrigin.Quality.AzimuthalGap,
+		MinimumDistance:       e.PreferredOrigin.Quality.MinimumDistance,
+		Magnitude:             e.PreferredMagnitude.Magnitude.Value,
+		MagnitudeUncertainty:  e.PreferredMagnitude.Magnitude.Uncertainty,
+		MagnitudeType:         e.PreferredMagnitude.Type,
+		MagnitudeStationCount: e.PreferredMagnitude.StationCount,
+	}
+
+	for _, v := range e.PreferredOrigin.Arrivals {
+		if v.Weight > 0.0 {
+			p := &quake.Phase{
+				Time: &quake.Timestamp{
+					Seconds: v.Pick.Time.Value.Unix(),
+					Nanos:   int64(v.Pick.Time.Value.Nanosecond()),
+				},
+				Phase:            v.Phase,
+				Residual:         v.TimeResidual,
+				Weight:           v.Weight,
+				Azimuth:          v.Azimuth,
+				Distance:         v.Distance,
+				NetworkCode:      v.Pick.WaveformID.NetworkCode,
+				StationCode:      v.Pick.WaveformID.StationCode,
+				LocationCode:     v.Pick.WaveformID.LocationCode,
+				ChannelCode:      v.Pick.WaveformID.ChannelCode,
+				EvaluationMode:   v.Pick.EvaluationMode,
+				EvaluationStatus: v.Pick.EvaluationStatus,
+			}
+
+			q.Phases = append(q.Phases, p)
+		}
+	}
+
+	for _, m := range e.PreferredOrigin.Magnitudes {
+		mag := &quake.Magnitude{
+			Magnitude:            m.Magnitude.Value,
+			MagnitudeUncertainty: m.Magnitude.Uncertainty,
+			Type:                 m.Type,
+			Method:               m.MethodID,
+			StationCount:         m.StationCount,
+		}
+
+		for _, v := range m.StationMagnitudeContributions {
+			if v.Weight > 0.0 {
+				s := &quake.StationMagnitude{
+					Weight:       v.Weight,
+					NetworkCode:  v.StationMagnitude.WaveformID.NetworkCode,
+					StationCode:  v.StationMagnitude.WaveformID.StationCode,
+					LocationCode: v.StationMagnitude.WaveformID.LocationCode,
+					ChannelCode:  v.StationMagnitude.WaveformID.ChannelCode,
+					Magnitude:    v.StationMagnitude.Magnitude.Value,
+					Type:         v.StationMagnitude.Type,
+					Amplitude:    v.StationMagnitude.Amplitude.Amplitude.Value,
+				}
+
+				mag.StationMagnitude = append(mag.StationMagnitude, s)
+			}
+		}
+		q.Magnitudes = append(q.Magnitudes, mag)
+	}
+
+	return q
+}
--- internal/seiscompml07/seiscompml07_test.go
+++ internal/seiscompml07/seiscompml07_test.go
@@ -1,14 +1,11 @@
 package seiscompml07
 
 import (
-	"github.com/gclitheroe/exp/internal/quake"
 	"io"
 	"os"
 	"path/filepath"
 	"testing"
 	"time"
-
-	"github.com/golang/protobuf/proto"
 )
 
 func TestUnmarshal(t *testing.T) {
@@ -275,154 +272,5 @@ func TestUnmarshal(t *testing.T) {
 
 	if e.ModificationTime().Format(time.RFC3339Nano) != "2015-10-12T22:46:41.228824Z" {
 		t.Errorf("Modification time expected 2015-10-12T22:46:41.228824Z got %s", e.ModificationTime().Format(time.RFC3339Nano))
-	}
-}
-
-func TestQuakeProto(t *testing.T) {
-	dir, cleanup, err := setup()
-	defer cleanup()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	f, err := os.Open(filepath.Join(dir, "2015p768477.pb"))
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer f.Close()
-
-	b, err := io.ReadAll(f)
-	if err != nil {
-		return
-	}
-
-	e := &quake.Quake{}
-	if err = proto.Unmarshal(b, e); err != nil {
-		t.Error(err)
-	}
-
-	if e.PublicID != "2015p768477" {
-		t.Errorf("expected publicID 2015p768477 got %s", e.PublicID)
-	}
-
-	if e.Type != "earthquake" {
-		t.Errorf("expected type earthquake got %s", e.Type)
-	}
-
-	if e.Agency != "WEL(GNS_Primary)" {
-		t.Errorf("Agency expected WEL(GNS_Primary) got %s", e.Agency)
-	}
-
-	if e.Time == nil {
-		t.Fatal("nil time")
-	}
-
-	if e.ModificationTime == nil {
-		t.Fatal("nil modification time")
-	}
-
-	if time.Unix(e.Time.Seconds, e.Time.Nanos).UTC().Format(time.RFC3339Nano) != "2015-10-12T08:05:01.717692Z" {
-		t.Errorf("time expected 2015-10-12T08:05:01.717692Z got %s", time.Unix(e.Time.Seconds, e.Time.Nanos).UTC().Format(time.RFC3339Nano))
-	}
-
-	if e.Latitude != -40.57806609 {
-		t.Errorf("Latitude expected -40.57806609 got %f", e.Latitude)
-	}
-	if e.LatitudeUncertainty != 1.922480006 {
-		t.Errorf("Latitude uncertainty expected 1.922480006 got %f", e.LatitudeUncertainty)
-	}
-
-	if e.Longitude != 176.3257242 {
-		t.Errorf("Longitude expected 176.3257242 got %f", e.Longitude)
-	}
-	if e.LongitudeUncertainty != 3.435738791 {
-		t.Errorf("Longitude uncertainty expected 3.435738791 got %f", e.LongitudeUncertainty)
-	}
-
-	if e.Depth != 23.28125 {
-		t.Errorf("Depth expected 23.28125 got %f", e.Depth)
-	}
-	if e.DepthUncertainty != 3.575079654 {
-		t.Errorf("Depth uncertainty expected 3.575079654 got %f", e.DepthUncertainty)
-	}
-
-	if e.Method != "NonLinLoc" {
-		t.Errorf("Method expected NonLinLoc got %s", e.Method)
-	}
-
-	if e.EarthModel != "nz3drx" {
-		t.Errorf("EarthModel expected NonLinLoc got %s", e.EarthModel)
-	}
-
-	if e.StandardError != 0.5592857863 {
-		t.Errorf("StandardError expected 0.5592857863 got %f", e.StandardError)
-	}
-
-	if e.AzimuthalGap != 166.4674465 {
-		t.Errorf("AzimuthalGap expected 166.4674465 got %f", e.AzimuthalGap)
-	}
-
-	if e.MinimumDistance != 0.1217162272 {
-		t.Errorf("MinimumDistance expected 0.1217162272 got %f", e.MinimumDistance)
-	}
-
-	if e.UsedPhaseCount != 44 {
-		t.Errorf("UsedPhaseCount expected 44 got %d", e.UsedPhaseCount)
-	}
-
-	if e.UsedStationCount != 32 {
-		t.Errorf("UsedStationCount expected 32 got %d", e.UsedStationCount)
-	}
-
-	if e.MagnitudeType != "M" {
-		t.Errorf("e.MagnitudeType expected M got %s", e.MagnitudeType)
-	}
-
-	if e.Magnitude != 5.691131913 {
-		t.Errorf("magnitude expected 5.691131913 got %f", e.Magnitude)
-	}
-
-	if e.MagnitudeUncertainty != 0 {
-		t.Errorf("uncertainty expected 0 got %f", e.MagnitudeUncertainty)
-	}
-
-	if e.MagnitudeStationCount != 171 {
-		t.Errorf("e.MagnitudeStationCount expected 171 got %d", e.MagnitudeStationCount)
-	}
-
-	if len(e.Magnitudes) != 3 {
-		t.Errorf("expected 3 magnitudes got %d", len(e.Magnitudes))
-	}
-
-	var found bool
-	for _, v := range e.Magnitudes {
-		if v.Type == "ML" {
-			found = true
-
-			if v.Magnitude != 6.057227661 {
-				t.Errorf("magnitude expected 6.057227661 got %f", v.Magnitude)
-			}
-			if v.MagnitudeUncertainty != 0.2576927171 {
-				t.Errorf("Uncertainty expected 0.2576927171 got %f", v.MagnitudeUncertainty)
-			}
-			if v.StationCount != 23 {
-				t.Errorf("v.StationCount expected 23 got %d", v.StationCount)
-			}
-			if v.Method != "trimmed mean" {
-				t.Errorf("v.Method expected trimmed mean got %s", v.Method)
-			}
-
-			if len(v.StationMagnitude) != 23 {
-				t.Errorf("station magnitudes expected 23 got %d", len(v.StationMagnitude))
-			}
-		}
-	}
-
-	if !found {
-		t.Error("did not find magnitude ML")
-	}
-
-	if len(e.Phases) != 44 {
-		t.Errorf("phases expected 44 got %d", len(e.Phases))
 	}
 }
